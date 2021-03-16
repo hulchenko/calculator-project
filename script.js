994fa9ce115e583bec6133bf0ddc88dcebfa2c5a
hulchenko <74687999+hulchenko@users.noreply.github.com>
2021-03-16 16:10:37 -0600

    tweak html and css, add some js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Your calculator is going to contain functions for all of the basic math operators you typically find on simple calculators, so start by creating functions for the following items and testing them in your browser’s console.
 const add = function (a, b) {
   return a + b;
 };
@@ -14,4 +15,9 @@ const divide = function (a, b) {
   return a / b;
 };
 
-console.log(divide(2, 4));
+//Create a new function operate that takes an operator and 2 numbers and then calls one of the above functions on the numbers.
+function operate(operator, a, b) {
+  return operator;
+}
+
+console.log(operate(subtract(2, 3)));
