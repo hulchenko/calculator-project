1ed46a0f095bdf5ff4b1e0f992aadd5940a0e0ec
hulchenko <74687999+hulchenko@users.noreply.github.com>
2021-03-17 19:18:09 -0600

    rebuild js, adjust html

--- script.js
+++ script.js
@@ -1,44 +1,59 @@
-//operators
-const add = function (a, b) {
-  return a + b;
-};
+const calculator = document.querySelector('.calculator');
+const screen = document.querySelector('.screen');
+const keys = document.querySelector('.btn-grid');
+const upperKeys = document.querySelector('.btn-grid-top');
+let firstValue = '';
+let secondValue = '';
 
-const subtract = function (a, b) {
-  return a - b;
-};
-
-const multiply = function (a, b) {
-  return a * b;
-};
-
-const divide = function (a, b) {
-  return a / b;
-};
-
-function operate(operator, a, b) {
-  return operator;
-}
-
-//variables
-const btn = Array.from(document.querySelectorAll('.btn'));
-
-const screen = document.getElementById('screen');
-
-///
-btn.map((button) => {
-  button.addEventListener('click', (e) => {
-    switch (e.target.innerHTML) {
-      default:
-        screen.innerHTML += e.target.innerHTML;
-        break;
-      case 'CLEAR':
-        screen.innerHTML = '';
-        break;
-      case '←':
-        screen.innerHTML = screen.innerHTML.slice(0, -1); //removes last character and returns a new array
-        break;
-      // case '+':
-      //   screen.innerHTML = add(screen.innerHTML, screen.innerHTML);
+upperKeys.addEventListener('click', function (e) {
+  if (e.target.matches('button')) {
+    const key = e.target;
+    const keyContent = e.target.innerHTML;
+    const action = key.dataset.action;
+    const screenContent = screen.innerHTML;
+    if (action === 'clear') {
+      //using screenContent wouldn't work, as it changing const variable value
+      screen.innerHTML = '0';
     }
-  });
+    if (action === 'backspace') {
+      screen.innerHTML = screen.innerHTML.slice(0, -1); //removes last character and returns a new array
+    }
+  }
+});
+
+keys.addEventListener('click', function (e) {
+  if (e.target.matches('button')) {
+    const key = e.target; //? probably don't need this
+    const keyContent = e.target.innerHTML; //num
+    const action = key.dataset.action; //data-action class value
+    const screenContent = screen.innerHTML;
+    const previousKeyType = calculator.dataset.previousKeyType;
+    if (!action) {
+      //any num button pressed
+      if (screenContent === '0' || previousKeyType === 'operator') {
+        screen.innerHTML = keyContent;
+      } else {
+        screen.innerHTML += keyContent;
+      }
+    }
+    if (
+      action === 'add' ||
+      action === 'subtract' ||
+      action === 'multiply' ||
+      action === 'divide'
+    ) {
+      key.classList.add('operator-click');
+      calculator.dataset.previousKeyType = 'operator';
+    }
+    if (action === 'decimal') {
+      screen.innerHTML = screenContent + '.';
+    }
+    if (action === 'calculate') {
+      const secondValue = screenContent;
+      console.log(secondValue);
+    }
+    Array.from(key.parentNode.children).map(function (k) {
+      k.classList.remove('operator-click');
+    });
+  }
 });
